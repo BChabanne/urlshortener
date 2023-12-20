1887b81ffe0891950b4cc0b6fd534097e5a195f6
Benjamin Chabanne <benjamin.chabanne.95@gmail.com>
2023-12-20 16:53:23 +0100

    feat: http hello world

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	err := http.ListenAndServe("127.0.0.1:8000", http.HandlerFunc(hello))
+	log.Fatal(err)
 }
